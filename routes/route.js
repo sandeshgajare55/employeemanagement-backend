472f9c86ff9b88a1ea36e5c9db99928ead68e803
sandeshgajare55 <sandeshgajare35@gmail.com>
2024-08-07 16:01:44 +0530

    API for Retriving User By ID

--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createUser,getUser,updateUser,deleteUser } from '../controller/usercontroller.js';
+import { createUser,getUser,updateUser,deleteUser,getUserById } from '../controller/usercontroller.js';
 const routers=express.Router()
 
 routers.post('/create',createUser)
 routers.get('/getuser',getUser)
 routers.put('/updateuser/:id',updateUser)
+routers.get('/getuserbyid/:id',getUserById)
 routers.delete('/deleteuser/:id',deleteUser)
 
 export default routers
